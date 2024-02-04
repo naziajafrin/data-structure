b175e8af2eefab891dcb4f7e6ffdc1b9f50ca127
Nazia <nazia.jafrin1@gmail.com>
2024-02-04 23:50:34 +0600

    vector initialize

---
+++ module_2/vector_initialization.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  // vector <int> v2(5, 200);
+  // vector <int> v(v2);
+  vector <int> v = {1,2,3,4,5};
+  cout << v.size() <<endl;
+  for(int i = 0; i < v.size(); i++){
+    cout << v[i] << endl;
+  }
+  return 0;
+}
