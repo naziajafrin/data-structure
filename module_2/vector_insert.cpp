6bdc706026a7bb9ca5ba6aaf4ec4d26bd2c9fc14
Nazia <nazia.jafrin1@gmail.com>
2024-02-06 11:22:39 +0600

    vector insert

--- module_2/vector_insert.cpp
+++ module_2/vector_insert.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 int main(){
   vector <int> x = {10, 20, 30, 40};
+  x.insert(x.begin()+2, 300);
   
+  vector <int> v = {9,6,7,9,5};
+  x.insert(x.begin()+3, v.begin(), v.end());
+  
+  for(int v:x){
+    cout << v << endl;
+  }
   return 0;
 }
